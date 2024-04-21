c0bb566db36f712264adf540540d09feca391b85
Syed Abrar Tirmiz <sabrar5000@gmail.com>
2024-04-22 04:33:55 +0500

    Namaz Timing

---
+++ app.js
@@ -0,0 +1,29 @@
+// Declaration
+const dataBtn = document.querySelector("#get-data");
+const cards = document.querySelector(".cards");
+const localData = new Data();
+
+const fajr_st = document.querySelector("#faj-start");
+const sr = document.querySelector("#sun-rise");
+const dhu_st = document.querySelector("#dhu-start");
+const asr_st = document.querySelector("#asr-start");
+const ss = document.querySelector("#sun-set");
+const mag_st = document.querySelector("#magh-start");
+const ish_st = document.querySelector("#ish-start");
+const tah_st = document.querySelector("#tah-start");
+
+const fajr_end = document.querySelector("#faj-end");
+const dhu_end = document.querySelector("#dhu-end");
+const asr_end = document.querySelector("#asr-end");
+const mag_end = document.querySelector("#magh-end");
+const ish_end = document.querySelector("#ish-end");
+const tah_end = document.querySelector("#tah-end");
+
+const fajr_rem = document.querySelector("#faj-rem");
+const dhu_rem = document.querySelector("#dhu-rem");
+const asr_rem = document.querySelector("#asr-rem");
+const mag_rem = document.querySelector("#magh-rem");
+const ish_rem = document.querySelector("#ish-rem");
+const tah_rem = document.querySelector("#tah-rem");
+
+// Functions
